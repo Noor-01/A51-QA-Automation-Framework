a2b7c73e7f84e953e54288a9dd11f1c7b351e893
Noor-01 <noor.alam@testpro.io>
2023-10-30 23:25:41 -0600

    Homework-18

---
+++ src/test/java/Homework18.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class Homework18 extends BaseTest {
+
+    @Test
+    public void playSong() throws InterruptedException {
+
+        navigateToPage();
+        provideEmail("demo@class.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
+        clickPlayNextSongButton();
+        clickPlaySongButton();
+        isSoundBarsDisplayed();
+    }
+}

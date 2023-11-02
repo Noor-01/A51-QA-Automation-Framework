3424921f77835375c71d015088eba82cdbe9cc94
Noor-01 <noor.alam@testpro.io>
2023-11-01 23:20:47 -0600

    Homework19

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,83 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
 
 public class BaseTest {
-
+    public WebDriver driver = null;
+    public String url = "https://qa.koel.app/";
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+    @BeforeMethod
+    @Parameters({"BaseURL"})
+    public void launchBrowser(String BaseURL) {
+        //Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        url = BaseURL;
+    }
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+    public void navigateToPage() {
+        driver.get(url);
+    }
+    public void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+    public void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+    public void clickSubmit() {
+        WebElement submit = driver.findElement(By.cssSelector("[type='submit']"));
+        submit.click();
+    }
+    public boolean isAvatarDisplayed() {
+        WebElement avatarIcon = driver.findElement(By.cssSelector("[class='avatar']"));
+        return avatarIcon.isDisplayed();
+    }
+    public void clickPlayNextSongButton() {
+        WebElement playNextSongBtn = driver.findElement(By.cssSelector("[data-testid='play-next-btn']"));
+        playNextSongBtn.click();
+    }
+    public void clickPlaySongButton() {
+        WebElement playSongBtn = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
+        playSongBtn.click();
+    }
+    public void isSoundBarsDisplayed() {
+        WebElement soundBars = driver.findElement(By.cssSelector("[alt='Sound bars']"));
+        Assert.assertTrue(soundBars.isDisplayed());
+    }
+    public void clickPlaylist() {
+        WebElement playList = driver.findElement(By.xpath("//section[@id='playlists']//li[6]"));
+        playList.click();
+    }
+    public void clickDeletePlaylistBtn() {
+        WebElement deletePlayList = driver.findElement(By.cssSelector("[class='del btn-delete-playlist']"));
+        deletePlayList.click();
+    }
+    public String deletedPlaylistConfirmationMessage() {
+        WebElement confirmationMessage = driver.findElement(By.cssSelector("div.success.show"));
+        return confirmationMessage.getText();
+    }
+
 }
